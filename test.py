bb2060e1dd5f0c178d196f52f3ac795dbd4fd64c
Lahiru <lahiru@tuta.io>
2024-08-08 19:04:52 +0530

    added all 3 devices with status check

--- test.py
+++ test.py
@@ -1,18 +1,46 @@
 import tinytuya
 
-# Connect to Device
-d = tinytuya.OutletDevice(
-    dev_id='',
-    address='Auto',
-    local_key="", 
-    version=3.4)
 
-# Get Status
-data = d.status() 
-print('set_status() result %r' % data)
+DEVICES = {
+    'Half Court A': {
+        'id': '',
+        'ip': 'Auto',
+        'key': ""
+    },
+    'Half Court B': {
+        'id': '',
+        'ip': 'Auto',
+        'key': ""
+    },
+    'Full Court': {
+        'id': '',
+        'ip': 'Auto',
+        'key': ""
+    }
+}
 
-# Turn On
-d.turn_on()
 
-# Turn Off
-d.turn_off()
+def check_device_status(name, device_info):
+    print(f"\nChecking status for {name}:")
+    
+
+    device = tinytuya.OutletDevice(
+        dev_id=device_info['id'],
+        address=device_info['ip'],
+        local_key=device_info['key'],
+        version=3.4
+    )
+    
+
+    status = device.status()
+    if 'dps' in status and '1' in status['dps']:
+        if status['dps']['1']:
+            print(f'{name} is ON')
+        else:
+            print(f'{name} is OFF')
+    else:
+        print(f'Failed to retrieve status for {name}')
+
+
+for name, info in DEVICES.items():
+    check_device_status(name, info)
